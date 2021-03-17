867c4457971256957a216ef3fa5aaac93c570706
lxx <lxx249@163.com>
2021-03-17 22:16:34 +0800

    创建vue项目

---
+++ 02创建vue项目/hello/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- 网页模块，编写html文件 -->
+  <div>
+    <h1 :title="message">hello vue project</h1>
+    <h2>{{message}}</h2>
+    <button @click="showHi">showHi</button>
+  </div>
+</template>
+
+<script>
+//  js代码
+export default {
+  data(){
+    return{
+      message: "hello vue"
+    }
+  },
+  methods:{
+    showHi(){
+      alert("h1")
+    }
+  }
+}
+</script>
+
+<style>
+
+/* css代码 */
+</style>
